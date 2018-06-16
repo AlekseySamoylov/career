7e1c9cb6a267f7a71b42c5b113e0a15bafd69a64
Aleksei Samoilov <alekseysamoylov89@gmail.com>
2018-06-16 18:52:33 +0300

    oauth2 login facebook

--- career-security/src/main/java/com/alekseysamoylov/career/security/SecurityApplication.java
+++ career-security/src/main/java/com/alekseysamoylov/career/security/SecurityApplication.java
@@ -2,10 +2,28 @@ package com.alekseysamoylov.career.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
-public class SecurityApplication {
+@EnableOAuth2Sso
+public class SecurityApplication extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
