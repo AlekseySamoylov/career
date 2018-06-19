190fef2a9424845fefcd6472c1782a77fbe85624
Aleksei Samoilov <alekseysamoylov89@gmail.com>
2018-06-19 10:48:10 +0300

    add Spock test examples

--- career-security/src/main/java/com/alekseysamoylov/career/security/service/UserService.java
+++ career-security/src/main/java/com/alekseysamoylov/career/security/service/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
     }
 
 
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
 }
--- career-security/src/test/java/com/alekseysamoylov/career/security/service/UserServiceTest.groovy
+++ career-security/src/test/java/com/alekseysamoylov/career/security/service/UserServiceTest.groovy
@@ -5,6 +5,7 @@ import com.alekseysamoylov.career.security.repository.UserRepository
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,5 +38,45 @@ class UserServiceTest extends Specification {
 
         then:
         thrown(IllegalArgumentException)
+    }
+
+    def "Should summarize values simple"() {
+        setup:
+        def userService = new UserService();
+
+        when:
+        def result = userService.sum(1, 1)
+
+        then:
+        result == 2
+    }
+
+    def "Should summarize values"() {
+        setup:
+        def userService = new UserService();
+
+        expect:
+        result == userService.sum(firstArg, lastArg)
+
+        where:
+        firstArg << [1, 2, 3]
+        lastArg << [1, 2, 3]
+        result << [2, 4, 6]
+    }
+
+    @Unroll("first: #firstArg plus last: #lastArg equals #result")
+    def "Should summarize values with table"() {
+        setup:
+        def userService = new UserService();
+
+        expect:
+        result == userService.sum(firstArg, lastArg)
+
+        where:
+        firstArg | lastArg || result
+        1        | 1       || 2
+        2        | 2       || 4
+        3        | 3       || 6
+        5        | 3       || 0
     }
 }
